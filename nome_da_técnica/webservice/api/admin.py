2eebf3fc8f02e8d1ed3122f714a09b67b04d3b4f
Erick Leal França <erick_franca1@hotmail.com>
2018-05-31 01:06:03 -0300

    não é mais necessário

--- nome_da_técnica/webservice/api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from api.models import SomarAPI
-
-admin.site.register(SomarAPI)
-
--- nome_da_técnica/webservice/somar/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SomarConfig(AppConfig):
-    name = 'somar'
